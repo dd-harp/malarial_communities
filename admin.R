12cbefa96e39db037917cb7bb6223b530a3cf372
Drew Dolgert <adolgert@uw.edu>
2019-09-30 18:34:17 -0700

    added reading landscan

--- admin.R
+++ admin.R
@@ -1,8 +1,15 @@
+library(raster)
+library(sf)
+
 # Using https://geocompr.robinlovelace.net/read-write.html
 read.shapes <- function(data.dir) {
-  list(africa=st_read(dsn = c(data.dir, "Africa.shp")))
+  list(
+    africa=st_read(dsn = file.path(data.dir, "Africa.shp")),
+    landscan=raster(file.path(data.dir, "LandScan Global 2017", "lspop2017"))
+    )
 }
 
 
 data.dir <- "./data"
-africa <- read.shapes(data.dir)$africa
+input.data <- read.shapes(data.dir)
+africa = input.data$africa
